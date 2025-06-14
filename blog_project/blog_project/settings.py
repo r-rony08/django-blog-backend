90118d3966e7f40fb5d1a3ab707ad69f720c54ca
r-rony08 <robiul.rony1008@gmail.com>
2025-06-14 23:51:22 +0600

    add requirment.txt and modify setting

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1nmxf#c6h@_7x(#uj+u_mbm$*bq0@_y$s_igmor+jcmsjv8s62'
+#SECRET_KEY = 'django-insecure-1nmxf#c6h@_7x(#uj+u_mbm$*bq0@_y$s_igmor+jcmsjv8s62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -74,12 +74,22 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
