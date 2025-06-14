636af9b75dbadee9068e14c587bf89f834beb30d
r-rony08 <robiul.rony1008@gmail.com>
2025-06-15 00:15:04 +0600

    Add Post,Comment,Category,PostCategory model + admin register

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,62 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+    #tags = models.ManyToManyField(Tag, related_name='posts')
+
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #categories = models.ManyToManyField(Category, related_name='posts')
+
+
+    class Meta:
+        unique_together = ('post', 'category')
+
+    def __str__(self):
+        return f"{self.post.title} -> {self.category.name}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+'''
+relationships:
+Post ➡ belongs to one User
+
+Post ➡ has many Comment
+
+Post ➡ can have many Category (via M2M)
+
+Post ➡ can have many Tag
+'''
