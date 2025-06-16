a9ba56b1af06f1bd3f1c0b866b3d9e5e8fb428f0
r-rony08 <robiul.rony1008@gmail.com>
2025-06-16 11:21:01 +0600

    Add JWT auth + register API

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,  # change to your preferred size
     'DEFAULT_PERMISSION_CLASSES': [
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -18,13 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import PostViewSet, CommentViewSet
+from blog.views_auth import RegisterView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterView.as_view(), name='register'),
 ]
